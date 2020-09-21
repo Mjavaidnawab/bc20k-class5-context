6391022e0c0e8805e01d1f29031f3a2580a7f9f1
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-09-21 22:33:29 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     <CounterContext.Provider value = {countState}>
       <div >
       
-      <h1> PIAIC Boot Camp 2020 - Class 05 - Context API createContext useContext</h1>
+      <h1> PIAIC Boot Camp 2020 - Class 05 - Context API and Reducer</h1>
       <h1> Student Name : Muhammad Javaid Nawab overseas</h1>
-      <h3> Github Repo: https://github.com/Mjavaidnawab/bc20k-class05  </h3>
-      <h3> Surge URL  : http://bc20k-class05.surge.sh/</h3>    
+      <h3> Github Repo: https://github.com/Mjavaidnawab/bc20k-class5-context  </h3>
+      <h3> Surge URL  : http://bc20k-class05-context.surge.sh/</h3>    
 
         <Parent />
       </div>
