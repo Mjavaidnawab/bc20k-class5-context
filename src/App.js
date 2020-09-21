5ced8afd52e8b449603a9a1d69c5d57a5c938bd3
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-09-21 23:12:19 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <h1> PIAIC Boot Camp 2020 - Class 05 - Context API and Reducer</h1>
       <h1> Student Name : Muhammad Javaid Nawab overseas</h1>
       <h3> Github Repo: https://github.com/Mjavaidnawab/bc20k-class5-context  </h3>
-      <h3> Surge URL  : http://bc20k-class05-context.surge.sh/</h3>    
+      <h3> Surge URL  : http://bc20k-class5-context.surge.sh/</h3>    
 
         <Parent />
       </div>
